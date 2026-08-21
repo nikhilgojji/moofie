69b0adc082e47388b2cdf6349c058033a1d49108
Nikhil Gojji <nikhilgojji@gmail.com>
2026-08-20 22:29:59 -0700

    Remove logo and simplify favicon

--- src/components/SiteChrome.jsx
+++ src/components/SiteChrome.jsx
@@ -66,7 +66,6 @@ export function PublicNav({ label = "Public page navigation" }) {
     <header className="access-header public-header">
       <a className="wordmark" href="/">
         <span>Moofie</span>
-        <img src="/bobcat.png" alt="" aria-hidden="true" />
       </a>
       <nav className="access-nav" aria-label={label}>
         <ThemeToggle />
@@ -112,7 +111,6 @@ export function SignedInNav({ data, user, disconnect, signOut, deleteAccount }) 
     <header className="topbar signed-in-nav">
       <a className="wordmark" href="/">
         <span>Moofie</span>
-        <img src="/bobcat.png" alt="" aria-hidden="true" />
       </a>
       <div className="signed-in-nav-actions">
         <ThemeToggle />
